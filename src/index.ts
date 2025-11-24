230b8bafddb180947476a4b3c6d7b20d02f89586
Ha-Mundo <trauzzu@gmail.com>
2025-11-24 21:11:47 +0000

    initial get route updated

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,29 @@ import type {Express} from 'express'
 const app:Express = express()
 const PORT = 8000
 
-app.listen(():void => console.log(`listening on port ${PORT}`))
+type Pet = {
+  name: string,
+  species: string,
+  adopted: boolean,
+  age: number
+}
+
+const pets:Pet[] = [
+  {
+    name: "Rubik",
+    species: "Cat",
+    adopted: true,
+    age: 3
+  }, {
+    name: "Pickle",
+    species: "Dog",
+    adopted: false,
+    age: 5
+  }
+]
+
+app.get('/', (req, res)=> {
+  res.json(pets)
+})
+
+app.listen(PORT, ():void => console.log(`listening on port ${PORT}`))
