57d4aee392d76ec14d111f30de37a0caf30eac47
Ha-Mundo <trauzzu@gmail.com>
2025-11-25 11:30:51 +0000

    implemented more specific responses + Pet type imported

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import type {Express, Request, Response} from 'express'
 import cors from 'cors'
 
 import {pets} from './data/pets'
+import type {Pet} from'./data/pets'
 
 const PORT = 8000
 const app:Express = express()
@@ -10,11 +11,11 @@ const app:Express = express()
 app.use(cors())
 
 
-app.get('/', (req:Request, res:Response): void => {
+app.get('/', (req:Request, res:Response<Pet[]>): void => {
   res.json(pets)
 })
 
-app.use((req: Request, res:Response):void => {
+app.use((req: Request, res:Response<{message:string}>):void => {
   res.status(404).json({message:"No route found!"})
 })
 
