b8a4837e0254ea5d2e221ebca41f56714daf4865
Ha-Mundo <trauzzu@gmail.com>
2025-11-25 10:04:44 +0000

    code refactored + import data

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,11 @@
 import express from 'express'
 import type {Express} from 'express'
+import {pets} from './data/pets'
 
 const app:Express = express()
 const PORT = 8000
 
-type Pet = {
-  name: string,
-  species: string,
-  adopted: boolean,
-  age: number
-}
 
-const pets:Pet[] = [
-  {
-    name: "Rubik",
-    species: "Cat",
-    adopted: true,
-    age: 3
-  }, {
-    name: "Pickle",
-    species: "Dog",
-    adopted: false,
-    age: 5
-  }
-]
 
 app.get('/', (req, res)=> {
   res.json(pets)
