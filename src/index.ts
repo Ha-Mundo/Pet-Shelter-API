eeda2b6163bcbcd732a228dcf1073fad43665855
Ha-Mundo <trauzzu@gmail.com>
2025-11-26 15:06:10 +0000

    implemented new route to get a single pet with more specific request

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,14 @@ const app:Express = express()
 
 app.use(cors())
 
-
 app.get('/', (req:Request, res:Response<Pet[]>): void => {
   res.json(pets)
+})
+
+app.get('/:id', (req:Request<{id:string}>, res:Response):void =>{
+  const {id} = req.params
+  const pet = pets.find(pet=> pet.id.toString() === id)
+  res.json(pet)
 })
 
 app.use((req: Request, res:Response<{message:string}>):void => {
