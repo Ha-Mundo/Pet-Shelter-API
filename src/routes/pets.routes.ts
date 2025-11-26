0994f89fca29224cc497766682bb31d9ed21c4bf
Ha-Mundo <trauzzu@gmail.com>
2025-11-26 21:01:31 +0000

    new middleware imported

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -3,10 +3,9 @@ import type {Router} from 'express'
 
 import {getPets, getPetById} from '../controllers/pets.controllers'
 
-import {validateNumericId} from '../middleware/pets.middleware'
+import {validateNumericId, pleaseAuth} from '../middleware/pets.middleware'
 
 export const petRouter:Router = express.Router()
 
 petRouter.get('/', getPets)
-
-petRouter.get('/:id',validateNumericId, getPetById) 
+petRouter.get('/:id',pleaseAuth, validateNumericId, getPetById)
